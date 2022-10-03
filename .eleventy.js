310fbff7d385e6fd4776fd70787266c8bf5cfaf6
Jason Kunesh <jdkunesh@gmail.com>
2022-10-03 15:04:00 -0500

    keeping up with the joneses

--- .eleventy.js
+++ .eleventy.js
@@ -4,8 +4,10 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 module.exports = function(eleventyConfig) {
 
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
-  eleventyConfig.addPassthroughCopy('src/img')
-
+  eleventyConfig.addPassthroughCopy('src/img');
+  eleventyConfig.addPassthroughCopy('src/favicon.*');
+  eleventyConfig.addPassthroughCopy('src/*.png');
+  eleventyConfig.addPassthroughCopy('src/site.webmanifest');
 
   const {
     DateTime
